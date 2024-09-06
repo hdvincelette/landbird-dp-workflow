70ee9e7b3746fd77757e236b004c431bac582439
Hannah <hdvincelette@gmail.com>
2024-09-05 17:19:47 -0800

    Update apps.R

--- R/apps.R
+++ R/apps.R
@@ -342,7 +342,7 @@ correct.colname.app <- function(data, invalid.col, unused.cols) {
 }
 
 
-select.value.app <- function(variable, invalid.values) {
+select.value.app <- function(variable, invalid.values, ref.dxnry) {
   runGadget(
     app = shinyApp(
       ui <- bootstrapPage(
@@ -360,23 +360,58 @@ select.value.app <- function(variable, invalid.values) {
                      "' contains entry values not found in the data dictionary.\n"
               )
             )),
-            shinyWidgets::awesomeCheckboxGroup(
+            shinyWidgets::prettyCheckboxGroup(
               "selected.values",
               h5("Select values to correct in the data."),
-              choices =  invalid.values
+              choices =  invalid.values,
+              outline = TRUE,
+              status = "warning"
             ),
             actionButton("action", "Submit"),
             br(),
             br(),
             hr(),
-            
+            h5(strong("Definition")),
+            htmlOutput("defs"),
+            br(),
+            h5(strong("Entry values")),
+            DT::DTOutput('domainItems', height = "375px")
           ),
         
         ), 
       
       server <- function(input, output, session) {
         observeEvent(input$action, stopApp())
-
+        
+        output$defs <- renderUI({
+          HTML(
+            paste(
+              ref.dxnry %>%
+                dplyr::filter(
+                  codeName == domainItem_value.table$Variable[b],
+                  domainItem_value == "dataField"
+                ) %>%
+                purrr::pluck("definition")
+            )
+          )
+        })
+        
+        output$domainItems = DT::renderDT({
+          ref.dxnry %>%
+            dplyr::filter(codeName == domainItem_value.table$Variable[b],
+                          domainItem_value != "dataField") %>%
+            dplyr::select("domainItem_value", "definition")
+        }, rownames = FALSE, 
+        options = list(
+          dom = 't',
+          lengthChange = FALSE,
+          autoWidth = FALSE,
+          headerCallback = htmlwidgets::JS(
+            "function(thead, data, start, end, display){",
+            "  $(thead).remove();",
+            "}"
+          )
+        ))
         
         observe({
           my_global_env <- globalenv()
@@ -389,5 +424,93 @@ select.value.app <- function(variable, invalid.values) {
   )
 }
 
-select.value.app(variable, invalid.values)
 
+correct.value.app <- function(variable, invalid.value, input.data, ref.dxnry) {
+  runGadget(
+    app = shinyApp(
+      ui <- bootstrapPage(
+        tags$head(tags$style(HTML("pre { overflow: auto; word-wrap: normal; }"))),
+        theme = theme.selection,
+        shinyjs::useShinyjs(),
+        input_dark_mode(id = "dark_mode", mode = "light"),
+        shinyjs::useShinyjs(),
+        br(),
+          column(
+            width = 12,
+            h5(strong(
+              paste0(
+                "The value '",
+                invalid.value,
+                "' is not listed in the data dictionary."
+              )
+            )),
+            shinyWidgets::radioGroupButtons(
+              "decision",
+              h5(strong("Choose an option.")),
+              choices =  c("keep", "edit","replace"),
+              direction = "vertical",
+              width = "200px",
+              checkIcon = list(yes = icon("ok", lib = "glyphicon"))
+            ),
+            
+            conditionalPanel(
+              condition = "input.decision == 'replace'",
+              shinyWidgets::pickerInput(
+                "val.choice",
+                h5("Select a dictionary value:"),
+                choices = ref.dxnry %>%
+                  dplyr::filter(codeName == domainItem_value.table$Variable[b],
+                                domainItem_value != "dataField") %>%
+                  purrr::pluck("domainItem_value"),
+                width = "300px",
+                options = list(
+                  `live-search` = TRUE,
+                  title = "Nothing selected",
+                  style = "btn-primary"
+                )
+              )
+            ),
+            conditionalPanel(
+              condition = "input.decision == 'edit'",
+              textInput(
+                "val.choice",
+                h5("Enter a replacement value:"),
+                value = invalid.value,
+                width = "300px"
+              )
+            ),
+            actionButton("action", "Submit"),
+            br(),
+            br()
+          ),
+        ), 
+      
+      server <- function(input, output, session) {
+        observeEvent(input$action, stopApp())
+      
+        decision <- reactive(input$decision)
+        val.choice <- reactive(input$val.choice)
+        
+        observe({
+          if (decision() %in% c("replace","edit") &
+              val.choice() == "" ) {
+            shinyjs::disable("action")
+          }
+          else{
+            shinyjs::enable("action")
+          }
+        })
+        
+        observe({
+          my_global_env <- globalenv()
+          my_global_env$decision<- input$decision
+          my_global_env$val.choice <-  input$val.choice
+        })
+      }
+    ),
+    
+    viewer = paneViewer(minHeight = 500)
+  )
+}
+
+correct.value.app(variable, invalid.value, input.data, ref.dxnry)
