faa6c821aee9eebfc4b771af42ab4cad0aca3342
Hannah <hdvincelette@gmail.com>
2025-02-22 17:09:02 -0900

    Update save.file.app.R

--- R/save.file.app.R
+++ R/save.file.app.R
@@ -1,93 +1,125 @@
-save.file.app <- function(auth.filename, new.data) {
+
+
+save.file.app <- function(dir, proposed.filename, ref.filename, format) {
+  if (missing(dir)) {
+    dir = "/*"
+  } else {
+    dir = paste0(gsub("/$","",dir),"/*")
+  }
+  if (missing(ref.filename)) {
+    ref.filename = ""
+  }
+  if (missing(format)) {
+    format = ""
+  } else {
+    format<- paste0(" ",format)
+  }
+  
   runGadget(
     app = shinyApp(
-      ui <- 
-        page(
+      ui <-
+        fluidPage(
           bootstrapPage(
-        tags$head(tags$style(
-          HTML("pre { overflow: auto; word-wrap: normal; }")
-        )),
-        theme = theme.selection,
-        shinyjs::useShinyjs(),
-        # input_dark_mode(id = "dark_mode", mode = "light"),
-        shinyjs::useShinyjs(),
-        br(),
-        navset_tab(nav_panel(
-          strong("Download"),
-        column(
-          width = 12,
-          h5(strong("Download the new data file.")),
-          textInput(
-            "new.name",
-            label =  h5("File name:"),
-            value = paste0(
-              gsub(
-                paste0("\\", vers.suffix, ".*"),
-                "",
-                auth.filename,
-                ignore.case = TRUE
+            theme = theme.selection,
+            shinyjs::useShinyjs(),
+            br(),
+            column(width = 12, uiOutput("dfmsg")),
+            br(),
+            column(
+              width = 12,
+              h4(strong(paste0("Export the new",format," file"))),
+              textInput( 
+                "new.name", 
+                "Filename:", 
+                value = proposed.filename
               ),
-              "_",
-              suppressWarnings(as.numeric(ifelse(
-                grepl(paste0("\\", vers.suffix), auth.filename, ignore.case = TRUE),
-                sub(paste0(".*\\", vers.suffix), "", auth.filename, ignore.case = TRUE),
-                ""
-              )))
-            ),
-            width = "300px"
-          ),
-          dipsaus::actionButtonStyled("reset", "Reset", width = "100px"
-                                      # , type ="danger"
-                                      ),
-          br(),
-          br(),
-          br(),
-          downloadButton("download", "Download"),
-          actionButton("action", "Done", width = "100px", class = "btn-primary"),
-          br(),
-          br()
-        )),
-        nav_panel(
-          strong("Review"), 
-          column(width = 12, 
-                 h5(strong("")), 
-                 DT::DTOutput('df', height = "350px"))
-        ))
-      )), 
+              span(textOutput("wrn"), style = "color:red"),
+              br(),
+              textInput("path", ""),
+              div(
+                style = "display: inline-block; vertical-align:center; horizontal-align:center",
+                class = "row-fluid",
+                actionButton("browse", "Choose directory", width = "150px"),
+                actionButton("action", "Export", width = "150px")
+              ),
+              br(),
+              br(),
+              htmlOutput("msg"),
+              br()
+            )
+          )
+        ),
+      
       server <- function(input, output, session) {
-        observeEvent(input$action, stopApp())
         
+        output$dfmsg <- renderUI(
+          HTML(paste("<em>",ref.filename,"</em>"))
+        )
+        
+        path <- reactive(input$path)
         new.name <- reactive(input$new.name)
+        selected.format <- reactive(input$selected.format)
+
         
+        shinyjs::hide("path")
         
-        observeEvent(input$reset, {
-          shinyjs::reset("new.name")
-          shinyjs::reset("new.name")
+        observe({
+          if (input$browse == 0)
+            return()
+        })
+        
+
+        output$wrn <- renderText({
+          if (input$browse != 0 & path() != "") {
+            if (paste0(tools::file_path_sans_ext(new.name()), ".csv") %in% list.files(path = path())) {
+            paste0("Warning: a file with this name already exists in the directory")
+            }
+          }
+        })
+      
+        output$msg <- renderUI({
+          if (input$browse != 0) {
+            HTML(paste("<b>Directory: </b>", path(), sep = '<br/>'))
+          }
+          
         })
         
         
-        output$df = DT::renderDataTable({
-          new.data %>% dplyr::select(tidyselect::all_of(selected.col()))
-        }, rownames = FALSE, options = list(
-          rowCallback = htmlwidgets::JS("function(r,d) {$(r).attr('height', '30px')}"),
-          lengthMenu = c(5, 10, 25, 50, 100),
-          pageLength = 100,
-          searchHighlight = TRUE,
-          columnDefs = list(list(width = "500px", className = 'dt-left', targets = "_all")),
-          scrollX = TRUE
-        ))
-        
-        
-        output$download <- downloadHandler(
-          filename = paste0(new.name(),".csv"),
-          content = function(file) {
-            write.csv(new.data, file, na = "", row.names = FALSE)
+        observeEvent(eventExpr = {
+          input$path
+        }, handlerExpr = {
+          if (path() == "") {
+            shinyjs::disable("action")
+          } else {
+            shinyjs::enable("action")
           }
-        )
+        })
         
+        
+        observeEvent(input$action, stopApp())
+        
+        
+        observe({
+          if (input$browse == 0)
+            return()
+          
+          updateTextInput(session,
+                          "path",
+                          value = utils::choose.dir(default = dir))
+          
+        })
+        
+        
+        observe({
+          my_global_env <- globalenv()
+          my_global_env$selected.dir <- path()
+          my_global_env$new.name <- new.name()
+        })
       }
     ),
-    
-    viewer = paneViewer(minHeight = 500)
+    viewer = paneViewer(minHeight  = 500)
   )
+  
 }
+
+
